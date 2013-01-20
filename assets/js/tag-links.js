edd57d161b224bbbd82ac6b18cacd4b9825d7378
Thomas Sarlandie <thomas@sarlandie.net>
2013-01-20 14:14:47 +0100

    Outbound and mailto link tagging now done in an unobtrusive and much simpler way

---
+++ assets/js/tag-links.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(function ($) {
+  var outbound = /^https?:\/\/(.*?)\//i;
+  var mailto = /^mailto:(.*)/i;
+  $('a').each( function( index ) {
+    var m;
+    if (this.href.indexOf(location.host) == -1 
+        && (m = outbound.exec(this.href))) 
+    {
+      $(this).click(function() {
+        _gaq.push(['_trackEvent', 'Outbound Traffic', m[1], this.href]);
+        setTimeout('document.location = "' + this.href + '"', 100);
+        return false;
+      });
+    }
+    if ((m = mailto.exec(this.href))) {
+      $(this).click(function() {
+        _gaq.push(['_trackEvent', 'Mailto', m[1], this.href]);
+        return true;
+      });
+    }
+  });
+});
